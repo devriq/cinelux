ddc12ed7a0d9a21b8c493690252a87a139dc062c
devriq <devriq.cordovil@gmail.com>
2022-06-23 18:14:07 -0300

    logo added

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import Listagem from "./Listagem";
 import Movie from "./Movie";
 
+import LOGO from './assets/cinelux.png';
+
 function NavBar(props: { movieList: Movie[], movieStatus: any[] }): JSX.Element {
     const [listNumber, setListNumber] = useState<string>('3');
-
 
     function handleClick(e: any): void {
         e.preventDefault();
@@ -13,9 +14,9 @@ function NavBar(props: { movieList: Movie[], movieStatus: any[] }): JSX.Element 
 
     return (
         <>
-        <header className="flex flex-col justify-evenly h-[10vh] items-center bg-[#111] mb-10">
+        <header className="flex flex-col justify-evenly h-[20vh] items-center bg-[#111] mb-10">
 
-            <img className="min-w-[100px] max-w-[200px]" id="logo" src='' alt="logIMGo" />
+            <img className="min-w-[100px] max-w-[200px] max-h-[10vh]" id="logo" src={LOGO} alt="logIMGo" />
             <ul className="flex-1 flex gap-4 items-center justify-center">
                 <li className="text-xs font-bold rounded overflow-hidden px-2 py-1 hover:text-green-300 active:bg-green-500 active:text-white" onClick={handleClick}>1960-1995</li>
                 <li className="text-xs font-bold rounded overflow-hidden px-2 py-1 hover:text-green-300 active:bg-green-500 active:text-white"  onClick={handleClick}>1996-2010</li>
