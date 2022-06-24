7645163700f5d86d920d4edb4bdab59bb399937b
devriq <devriq.cordovil@gmail.com>
2022-06-23 23:32:52 -0300

    round card added

--- src/Listagem.tsx
+++ src/Listagem.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import Card from "./Card";
 import Movie from "./Movie"
 import MovieShow from "./MovieShow"
 
--- src/MovieShow.tsx
+++ src/MovieShow.tsx
@@ -6,17 +6,19 @@ export default function MovieShow(props: {movie: Movie; status:any[]}): JSX.Elem
   const moviePoster = "https://image.tmdb.org/t/p/w500" + props.movie.poster_path
 
   return (
-    <div className="movieShow w-[200px] h-[350px] flex flex-col group">
-      <img className={`${movieState} h-[300px] border-2 border-green-300 group-hover:grayscale-0 transition-colors`} src={moviePoster} alt="poster"/>
-      {/* <div className='overviewInfo flex w-full h-[50px] leading-relaxed'>
-        <strong className='text-xs leading-relaxed flex-1 justify-center items-center'>
+    <div className={`${movieState} movieShow w-[200px] h-[300px] border-2 border-green-300 flex flex-col items-center rounded-xl overflow-hidden hover:grayscale-0`}>
+      <img className={`h-[300px] transition-colors`} src={moviePoster} alt="poster"/>
+      <div className='overviewInfo grid leading-relaxed text-xs mx-auto items-center card__description leading-relaxed w-[28ch]'>
+        <strong className='text-sm leading-relaxed flex-1 justify-center items-center p-2'>
           {props.movie.title}   
         </strong>
-        <span className='text-bold text-green-500 w-[50px] items-center justify-center text-center'>
+        {/* <span className='text-bold text-green-500 w-[50px] items-center justify-center text-center p-2'>
            {props.movie.release_date.substring(0, 4)}
-        </span> */}
-        {/* <p>{props.movie.overview}</p>
-      </div> */}
+        </span>  */}
+         <p className='text-[8px] text-justify leading-relaxed w-full p-2'>
+          {props.movie.overview}
+         </p>
+      </div> 
     </div>
   );
 }
